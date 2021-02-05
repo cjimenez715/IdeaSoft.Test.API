58d695832e49a24ed06ac368255bfb162ea15467
Cristhian Jimenez <cristhian.jimenez@iatec.com>
2021-02-04 21:14:40 -0400

    StartUp was updated

--- src/IdeaSoft.Test.Api/Startup.cs
+++ src/IdeaSoft.Test.Api/Startup.cs
@@ -3,6 +3,7 @@ using IdeaSoft.Test.Api.Data;
 using IdeaSoft.Test.Api.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -51,6 +52,10 @@ namespace IdeaSoft.Test.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello IdeaSoft!");
+                });
             });
         }
     }
